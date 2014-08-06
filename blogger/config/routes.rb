f8ea109c6bd544eeeceeb78519ce28b14c7d3d8a
Camsbury <camsbury7@gmail.com>
2014-08-06 10:41:40 -0400

    finished blog comments feature

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 	#The tutorial says Blogger::Application here 
 	#instead of Rails.application
 	root to: 'articles#index'
-	resources :articles
+	resources :articles do
+		resources :comments
+	end
 end
